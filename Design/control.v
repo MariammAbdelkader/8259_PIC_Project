b631e940b95afc2d65cea501e33ef2b38f41d201
farah-salama <farahhsalamaa@gmail.com>
2023-12-28 00:09:21 +0200

    Update control.v

--- Design/control.v
+++ Design/control.v
@@ -7,7 +7,7 @@ module control (
     inout [2:0] cascade_io, //cascade bus
 
     // internal bus
-    input [7:0] data_bus,
+    input [7:0] internal_data_bus,
     input write_ICW1, write_ICW2_4,
     input write_OCW1, write_OCW2, write_OCW3,
     input read,
@@ -19,16 +19,13 @@ module control (
     input [7:0] highest_level_in_service,
 
     //interrupt control signals
-    output [7:0] int_mask, //interrupt mask
-    output [7:0] eoi, //end of interrupt
+    output reg [7:0] int_mask, //interrupt mask
+    output reg [7:0] eoi, //end of interrupt
     output [2:0] priority_rotate,
-    output latch_in_service,
-    output  reg    level_edge_triggered,
-    /*output read_reg_en, 
-    output read_register_isr_or_irr,
-
-    ...
-    */
+    output reg latch_in_service,
+    output reg    level_edge_triggered,
+    output reg read_reg_en, 
+    output reg read_reg_isr_or_irr
 );
     reg single_or_cascade;
     reg set_icw4;
@@ -41,6 +38,7 @@ module control (
     reg   [7:0]   cascade_config;
 
     reg cascade_slave;
+    reg cascade_slave_enable;
 
     wire [7:0] acknowledge_interrupt;
 
@@ -49,16 +47,13 @@ module control (
     reg [1:0] command_state;
     reg [1:0] next_command_state;
 
-    localparam CMD_WRITE_READY = 2'b00;
+    localparam CMD_READY = 2'b00;
     localparam CMD_WRITE_ICW2 = 2'b01;
     localparam CMD_WRITE_ICW3 = 2'b10;
     localparam CMD_WRITE_ICW4 = 2'b11;
 
-    always @(next_command_state or posedge reset) begin
-        if(reset)
-            command_state <= CMD_READY;
-        else
-            command_state <= next_command_state;
+    always @(next_command_state) begin
+        command_state <= next_command_state;
     end
 
     // Initialization fsm
@@ -104,7 +99,7 @@ module control (
     reg [1:0] next_control_state;
 
     localparam CTRL_READY = 2'b00;
-    localparam POLL = 2'b01;
+    //localparam POLL = 2'b01;
     localparam ACK = 2'b10;
     //localparam ACK2 = 3'b011;
     //localparam ACK3 = 3'b100;
@@ -115,9 +110,7 @@ module control (
     always @(*) begin
         case (control_state)
             CTRL_READY: begin
-                if(write_ocw3_reg && data_bus[2])
-                    next_control_state = POLL;
-                else if (write_ocw2_reg || !int_ack)
+                if (write_ocw2_reg || !int_ack)
                     next_control_state = CTRL_READY;
                 else 
                     next_control_state = ACK;
@@ -128,20 +121,12 @@ module control (
                 else 
                     next_control_state = CTRL_READY;
             end
-            POLL: begin
-                if (read) 
-                    next_control_state = POLL;
-                else
-                    next_control_state = CTRL_READY;
-            end
             default: next_control_state = CTRL_READY;
         endcase
     end
 
     always @(*) begin
-        if (reset)
-            control_state <= CTRL_READY;
-        else if (write_ICW1)
+        if (write_ICW1)
             control_state <= CTRL_READY;
         else
             control_state <= next_control_state;
@@ -150,7 +135,7 @@ module control (
     always @* begin
         if (write_ICW1 == 1'b1)
             latch_in_service = 1'b0;
-        else if ((control_state == CTRL_READY) && (next_control_state == POLL))
+        else if ((control_state == CTRL_READY))
             latch_in_service = 1'b1;
         else if (cascade_slave == 1'b0)
             latch_in_service = (control_state == CTRL_READY) & (next_control_state != CTRL_READY);
@@ -159,8 +144,8 @@ module control (
     end
 
     // End of acknowledge sequence
-    wire    end_of_ack_seq =  (control_state != POLL) & (control_state != CTRL_READY) & (next_control_state == CTRL_READY);
-    wire    end_of_poll_command = (control_state == POLL) & (control_state != CTRL_READY) & (next_control_state == CTRL_READY);
+    wire    end_of_ack_seq =  (control_state != CTRL_READY) & (next_control_state == CTRL_READY);
+    wire    end_of_poll_command = (control_state != CTRL_READY) & (next_control_state == CTRL_READY);
 
 
         //...
@@ -170,20 +155,16 @@ module control (
     
     // A7-A5
     always @* begin
-        if (reset)
-            interrupt_vector_address[2:0] <= 3'b000;
-        else if (write_ICW1 == 1'b1)
-            interrupt_vector_address[2:0] <= data_bus[7:5];
+        if (write_ICW1 == 1'b1)
+            interrupt_vector_address[2:0] <= internal_data_bus[7:5];
         else
             interrupt_vector_address[2:0] <= interrupt_vector_address[2:0];
     end
 
     // LTIM
     always @* begin
-        if (reset)
-            level_edge_triggered <= 1'b0;
-        else if (write_ICW1 == 1'b1)
-            level_edge_triggered <= data_bus[3];
+        if (write_ICW1 == 1'b1)
+            level_edge_triggered <= internal_data_bus[3];
         else
             level_edge_triggered <= level_edge_triggered;
     end
@@ -191,30 +172,24 @@ module control (
     // ADI
     //call address interval 4 or 8 configureation
     always @* begin
-        if (reset)
-            call_address_interval <= 1'b0;
-        else if (write_ICW1 == 1'b1)
-            call_address_interval <= data_bus[2];
+        if (write_ICW1 == 1'b1)
+            call_address_interval <= internal_data_bus[2];
         else
             call_address_interval <= call_address_interval;
     end
 
     // SNGL
     always @* begin
-        if (reset)
-            single_or_cascade <= 1'b0;
-        else if (write_ICW1 == 1'b1)
-            single_or_cascade <= data_bus[1];
+        if (write_ICW1 == 1'b1)
+            single_or_cascade <= internal_data_bus[1];
         else
             single_or_cascade <= single_or_cascade;
     end
 
     //IC4
     always @* begin
-        if (reset)
-            set_icw4 <= 1'b0;
-        else if (write_ICW1 == 1'b1)
-            set_icw4 <= data_bus[0];
+        if (write_ICW1 == 1'b1)
+            set_icw4 <= internal_data_bus[0];
         else
             set_icw4 <= set_icw4;
     end
@@ -223,10 +198,10 @@ module control (
     // ICW 2 initialization
     //
     always @* begin
-        if (reset || write_ICW1 == 1'b1)
+        if (write_ICW1 == 1'b1)
             interrupt_vector_address[10:3] <= 3'b000;
         else if (write_icw2 == 1'b1)
-            interrupt_vector_address[10:3] <= data_bus;
+            interrupt_vector_address[10:3] <= internal_data_bus;
         else
         //maintain current bits
             interrupt_vector_address[10:3] <= interrupt_vector_address[10:3];
@@ -236,10 +211,10 @@ module control (
     // ICW 3
     //
     always @* begin
-        if (reset || write_ICW1 == 1'b1)
+        if (write_ICW1 == 1'b1)
             cascade_config <= 8'b00000000;
         else if (write_icw3 == 1'b1)
-            cascade_config <= data_bus;
+            cascade_config <= internal_data_bus;
         else
             cascade_config <= cascade_config;
     end
@@ -248,26 +223,26 @@ module control (
     //ICW 4 initialization
     //
     // BUF
-    always @(*) begin
+    /*always @(*) begin
         if (reset || write_ICW1)
             buff_mode_config <= 1'b0;
         else if (write_icw4 == 1'b1)
-            buff_mode_config <= data_bus[3];
-    end
+            buff_mode_config <= internal_data_bus[3];
+    end*/
 
     assign  SP_EN = ~buff_mode_config;
 
     // M/S
     always @(*) begin
-        if (reset || write_ICW1)
+        if (write_ICW1)
             buff_master_or_slave_config <= 1'b0;
         else if (write_icw4)
-            buff_master_or_slave_config <= data_bus[2];
+            buff_master_or_slave_config <= internal_data_bus[2];
     end
 
     // AEOI
     always @(*) begin
-        if (reset || write_ICW1)
+        if (write_ICW1)
             auto_eoi_config <= 1'b0;
         else if (write_icw4)
             auto_eoi_config <= internal_data_bus[1];
@@ -276,12 +251,10 @@ module control (
     //Operation Control Word 1
 
     always @(*) begin
-        if (reset)
-            int_mask <= 8'b11111111;
-        else if (write_initial_command_word_1 == 1'b1)
+        if (write_ICW1 == 1'b1)
             int_mask <= 8'b11111111;
         else if (write_ocw1_reg == 1'b1)
-            int_mask <= data_bus;
+            int_mask <= internal_data_bus;
         else
             int_mask <= int_mask;
     end
@@ -296,14 +269,30 @@ module control (
         else if (end_of_ack_seq == 1'b1)
             eoi = acknowledge_interrupt;
         else if (write_OCW2) begin
-            casez (data_bus[6:5])
+            casez (internal_data_bus[6:5])
                 2'b01:   eoi = highest_level_in_service; 
-                //2'b11:   eoi = num2bit(data_bus[2:0]);
+                //2'b11:   eoi = num2bit(internal_data_bus[2:0]);
                 default: eoi = 8'b00000000;
             endcase
         end
         else
             eoi = 8'b00000000;
+    end
+
+    // RR/RIS
+    always @(*) begin
+        if (write_ICW1 == 1'b1) begin
+            read_reg_en <= 1'b1;
+            read_reg_isr_or_irr <= 1'b0;
+        end
+        else if (write_ocw3_reg == 1'b1) begin
+            read_reg_en <= internal_data_bus[1];
+            read_reg_isr_or_irr <= internal_data_bus[0];
+        end
+        else begin
+            read_reg_en <= read_reg_en;
+            read_reg_isr_or_irr <= read_reg_isr_or_irr;
+        end
     end
 
 endmodule
