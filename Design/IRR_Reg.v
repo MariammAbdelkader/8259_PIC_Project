e02834e75404a3ac03c3be945b62ec03475a8e4a
Mariz-Youssef <131456558+Mariz-Youssef@users.noreply.github.com>
2024-01-02 01:26:17 +0200

    Update IRR_Reg.v

--- Design/IRR_Reg.v
+++ Design/IRR_Reg.v
@@ -1,21 +1,33 @@
 module IRR(
   input wire [7:0] interrupt_Requests,
-  //input from control logic
-  input[7:0] clear_IRR,
-  //input wire rst,
+  input [7:0] clear_IRR,
+  input freeze,
   output reg [7:0] IRR_Output
 );
- 
+
+// to store the output after the change of the clear signal
+reg[7:0] store_request;
+assign store_request=interrupt_Requests;
+  
   // Combinational logic for updating IRR
-  always @(interrupt_Requests or clear_IRR) begin
+  always @(*) begin
 
-// Reset only the bits that will be served at that time
-    if (clear_IRR) 
-      IRR_Output=IRR_Output& (~clear_IRR);
+    // If freeze is active, maintain the current IRR state
+    if (freeze) begin
+     // no operation
+    end
 
+    // Reset only the bits that will be served at that time
+    else if (clear_IRR) begin
+      //store_request = interrupt_Requests;
+      IRR_Output = store_request & (~clear_IRR);
+      store_request = IRR_Output;
+    end
+
+    // Update IRR based on incoming interrupt requests
     else 
-      // Update IRR based on incoming interrupt requests
-      IRR_Output <= interrupt_Requests;
+      IRR_Output = interrupt_Requests;
+ 
   end
 
 endmodule
