3627bc008ecbd8fd1ba179c6fc31fa27b0a64527
Mariz-Youssef <131456558+Mariz-Youssef@users.noreply.github.com>
2024-01-01 20:53:26 +0200

    Update IRR_tb.v

--- Test Bench/IRR_tb.v
+++ Test Bench/IRR_tb.v
@@ -1,30 +1,27 @@
-// TestBench
 module IRR_tb;
 
   // Inputs
   reg [7:0] interrupt_Requests;
-  reg rst;
+  reg [7:0] clear_bits;
 
   // Outputs
   wire [7:0] IRR_Output;
 
   // Instantiate the module under test
-  IRR2 test (
+  IRR test (
     .interrupt_Requests(interrupt_Requests),
-    .rst(rst),
+    .clear_IRR(clear_bits),
     .IRR_Output(IRR_Output)
   );
 
   // Initial stimulus
   initial begin
     // Initialize inputs
-    interrupt_Requests = 8'b00000010;  // Initial interrupt requests
-    rst = 1;
-    #10 rst = 0;  // Assert reset
-    #10 interrupt_Requests = 8'b01000000;
-    #10 interrupt_Requests = 8'b01001000;
-    #10 rst = 1;  // Release Reset
-    #10 rst = 0;
+    interrupt_Requests = 8'b00010010;  // Initial interrupt requests
+    #10 clear_bits = 8'b00000010;
+    #10 clear_bits = 8'b00010000;
+    #10 interrupt_Requests = 8'b10000000;
+    #10 clear_bits = 8'b10000000;
 
     #100 $stop;  // Stop simulation after some time
   end
