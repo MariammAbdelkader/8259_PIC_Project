f2f37b2cda7f15af98d9608f6e55e2ff73fc1b5b
Jasmine <112454290+JasmineMamdouh@users.noreply.github.com>
2024-01-02 22:34:42 +0200

    Update ControlBus_tb.v

--- Test Bench/ControlBus_tb.v
+++ Test Bench/ControlBus_tb.v
@@ -3,17 +3,15 @@
 module ControlBus_tb;
 
   // Inputs
-  reg reset;
   reg CS, rd_enable, wr_enable, A1;
   reg [7:0] bi_data_bus;
 
   // Outputs
   wire [7:0] internal_bus;
-  wire write_ICW_1, write_ICW2, write_ICW4, write_OCW1, write_OCW2, write_OCW3, read;
+  wire write_ICW_1, write_ICW2_4, write_OCW1, write_OCW2, write_OCW3, read;
 
   // Instantiate the module under test
   ControlBus dut (
-    .reset(reset),
     .CS(CS),
     .rd_enable(rd_enable),
     .wr_enable(wr_enable),
@@ -21,8 +19,7 @@ module ControlBus_tb;
     .bi_data_bus(bi_data_bus),
     .internal_bus(internal_bus),
     .write_ICW_1(write_ICW_1),
-    .write_ICW2(write_ICW2),
-    .write_ICW4(write_ICW4),
+    .write_ICW2_4(write_ICW2_4),
     .write_OCW1(write_OCW1),
     .write_OCW2(write_OCW2),
     .write_OCW3(write_OCW3),
@@ -32,21 +29,71 @@ module ControlBus_tb;
   // Initial block for test bench initialization
   initial begin
     // Initialize inputs
-    reset = 0;
-    CS = 1;
+    
+/*    CS = 1;
     rd_enable = 1;
     wr_enable = 1;
     A1 = 1;
-    bi_data_bus = 8'b10101010; // Some initial data
+    bi_data_bus = 8'b10111010; // Some initial data
 
     // Apply stimulus, Assuming some delay between operations
-    #10 reset = 1; // Assert reset
-    #10 reset = 0; // De-assert reset
+    #10 CS = 1;
     #10 CS = 0;  // Enable the chip
     #10 wr_enable = 0;  // Write operation
-    #10 CS = 1;  // Disable the chip
+    #10 A1 = 0;
+    #10 A1 = 1;
+    #10 bi_data_bus = 8'b11100001;
+    #10 bi_data_bus = 8'b11101000;
     #10 rd_enable = 0;  // Read operation
+    #10 CS = 1;
+
+*/
+ 	CS =1'b1;
+        rd_enable=1'b1;
+        wr_enable=1'b1;
+        A1=1'b0;
+        bi_data_bus= 8'b00000000;
+              
+       // Task : Write data
+        //ICW1
+       #10 CS=1'b0;
+       wr_enable=1'b0;
+       A1=1'b0;
+       bi_data_bus= 8'b00010011; 
+
+       //ICW2
+       #10 CS=1'b0;
+       wr_enable=1'b0;
+       A1=1'b1;
+       bi_data_bus= 8'b00010000;  
+       //ICW3
+
+       //ICW4
+       #10 CS=1'b0;
+       wr_enable=1'b0;
+       A1=1'b1;
+       bi_data_bus= 8'b00000X01;
+
+	//OCW1
+	#10 CS=1'b0;
+	wr_enable = 1'b0;
+	A1 = 1'b1;
+	bi_data_bus = 8'b00000000;
+
+	//OCW2
+	#10 CS=1'b0;
+	wr_enable = 1'b0;
+	A1 = 1'b0;
+	bi_data_bus = 8'b00000000;		//lsb 3 bits??IR level to be acted upon
+
+	//OCW3
+	#10 CS=1'b0;
+	wr_enable = 1'b0;
+	A1 = 1'b0;
+	bi_data_bus = 8'b00001000;
+	#10;
 
   end
+
 
 endmodule
